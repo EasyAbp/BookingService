714634bf101040d1ed87cc17465c9cf1b17e1aa4
gdlcf88 <gdlcf88@outlook.com>
2022-05-10 22:33:48 +0800

    Create `OccupyAssetEto` and `AssetOccupancyResultEto`

---
+++ src/EasyAbp.BookingService.Domain.Shared/EasyAbp/BookingService/AssetOccupancies/AssetOccupancyResultEto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.ObjectExtending;
+
+namespace EasyAbp.BookingService.AssetOccupancies;
+
+public class AssetOccupancyResultEto : ExtensibleObject, IMultiTenant
+{
+    public Guid? TenantId { get; set; }
+    
+    public Guid AssetId { get; set; }
+    
+    public string AssetName { get; set; }
+    
+    public string AssetDefinitionName { get; set; }
+    
+    public bool Success { get; set; }
+    
+    public virtual DateTime Date { get; protected set; }
+    
+    public virtual TimeSpan StartingTime { get; protected set; }
+    
+    public virtual TimeSpan Duration { get; protected set; }
+    
+    public virtual Guid? OccupierUserId { get; protected set; }
+    
+    public virtual string OccupierName { get; protected set; }
+}
---
+++ src/EasyAbp.BookingService.Domain.Shared/EasyAbp/BookingService/AssetOccupancies/OccupyAssetEto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.ObjectExtending;
+
+namespace EasyAbp.BookingService.AssetOccupancies;
+
+public class OccupyAssetEto : ExtensibleObject, IMultiTenant
+{
+    public Guid? TenantId { get; set; }
+    
+    public Guid AssetId { get; set; }
+    
+    public virtual DateTime Date { get; protected set; }
+    
+    public virtual TimeSpan StartingTime { get; protected set; }
+    
+    public virtual TimeSpan Duration { get; protected set; }
+    
+    public virtual Guid? OccupierUserId { get; protected set; }
+}
