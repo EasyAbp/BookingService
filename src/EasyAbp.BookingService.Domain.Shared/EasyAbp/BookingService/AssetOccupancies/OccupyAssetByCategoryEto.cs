a2665abd9f98d95dec461556ede17dde54a291c2
gdlcf88 <gdlcf88@outlook.com>
2022-05-12 03:01:02 +0800

    Create `OccupyAssetByCategoryEto` and `BulkOccupyAssetByCategoryEto`

---
+++ src/EasyAbp.BookingService.Domain.Shared/EasyAbp/BookingService/AssetOccupancies/OccupyAssetByCategoryEto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.ObjectExtending;
+
+namespace EasyAbp.BookingService.AssetOccupancies;
+
+/// <summary>
+/// Extra properties will fully map to <see cref="AssetOccupancyResultEto"/> by default.
+/// </summary>
+public class OccupyAssetByCategoryEto : ExtensibleObject, IMultiTenant
+{
+    public Guid? TenantId { get; set; }
+
+    public OccupyAssetByCategoryInfoModel Model { get; set; }
+
+    protected OccupyAssetByCategoryEto()
+    {
+    }
+
+    public OccupyAssetByCategoryEto(Guid? tenantId, OccupyAssetByCategoryInfoModel model)
+    {
+        TenantId = tenantId;
+        Model = model;
+    }
+}
