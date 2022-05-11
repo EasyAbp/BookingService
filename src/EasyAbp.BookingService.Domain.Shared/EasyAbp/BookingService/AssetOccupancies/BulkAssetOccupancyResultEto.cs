d095b7c18d39371e203386c0051882a85d937852
gdlcf88 <gdlcf88@outlook.com>
2022-05-11 20:04:14 +0800

    Add BulkAssetOccupancyResultEto and BulkOccupyAssetEto

---
+++ src/EasyAbp.BookingService.Domain.Shared/EasyAbp/BookingService/AssetOccupancies/BulkAssetOccupancyResultEto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.ObjectExtending;
+
+namespace EasyAbp.BookingService.AssetOccupancies;
+
+public class BulkAssetOccupancyResultEto : ExtensibleObject, IMultiTenant
+{
+    public Guid? TenantId { get; set; }
+
+    public bool Success { get; set; }
+
+    public List<AssetOccupancyInfoModel> Models { get; set; }
+}
---
+++ src/EasyAbp.BookingService.Domain.Shared/EasyAbp/BookingService/AssetOccupancies/BulkOccupyAssetEto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.ObjectExtending;
+
+namespace EasyAbp.BookingService.AssetOccupancies;
+
+public class BulkOccupyAssetEto : ExtensibleObject, IMultiTenant
+{
+    public Guid? TenantId { get; set; }
+
+    public List<OccupyAssetInfoModel> Models { get; set; }
+
+    protected BulkOccupyAssetEto()
+    {
+    }
+
+    public BulkOccupyAssetEto(Guid? tenantId, List<OccupyAssetInfoModel> models)
+    {
+        TenantId = tenantId;
+        Models = models;
+    }
+}
