d8a53c7290194e8340a2923eed7a8322fea6d492
gdlcf88 <gdlcf88@outlook.com>
2022-06-09 13:44:26 +0800

    Add a new EF Core migration for the Volume properties Resolve #7

---
+++ host/EasyAbp.BookingService.Web.Unified/Migrations/20220609053940_AddedVolume.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyAbp.BookingService.Migrations
+{
+    public partial class AddedVolume : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Volume",
+                table: "EasyAbpBookingServiceAssets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Volume",
+                table: "EasyAbpBookingServiceAssetOccupancies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Volume",
+                table: "EasyAbpBookingServiceAssets");
+
+            migrationBuilder.DropColumn(
+                name: "Volume",
+                table: "EasyAbpBookingServiceAssetOccupancies");
+        }
+    }
+}
