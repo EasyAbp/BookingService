98e691ea0709e09040aaac7390cf293a3ee51dee
RayMMond <uniquesyp@126.com>
2022-06-19 23:15:29 +0800

    Add ef core unit tests. Add asset category app service unit test.

--- test/EasyAbp.BookingService.Application.Tests/BookingServiceApplicationTestBase.cs
+++ test/EasyAbp.BookingService.Application.Tests/BookingServiceApplicationTestBase.cs
@@ -1,9 +1,45 @@
-﻿namespace EasyAbp.BookingService;
+﻿using System.Collections.Generic;
+using EasyAbp.BookingService.AssetDefinitions;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Guids;
+
+namespace EasyAbp.BookingService;
 
 /* Inherit from this class for your application layer tests.
  * See SampleAppService_Tests for example.
  */
 public abstract class BookingServiceApplicationTestBase : BookingServiceTestBase<BookingServiceApplicationTestModule>
 {
+    protected readonly IGuidGenerator GuidGenerator;
 
+    protected static readonly AssetDefinition AssetDefinition = new(nameof(AssetDefinition),
+        default,
+        new TimeInAdvance
+        {
+            MaxDaysInAdvance = 5
+        });
+
+    protected static readonly AssetDefinition AnotherAssetDefinition = new(nameof(AnotherAssetDefinition),
+        default,
+        new TimeInAdvance
+        {
+            MaxDaysInAdvance = 10
+        });
+
+    protected BookingServiceApplicationTestBase()
+    {
+        GuidGenerator = GetRequiredService<IGuidGenerator>();
+    }
+
+    protected override void AfterAddApplication(IServiceCollection services)
+    {
+        services.Configure<BookingServiceOptions>(options =>
+        {
+            options.AssetDefinitionConfigurations = new List<AssetDefinition>
+            {
+                AssetDefinition,
+                AnotherAssetDefinition
+            };
+        });
+    }
 }
--- test/EasyAbp.BookingService.EntityFrameworkCore.Tests/EntityFrameworkCore/PeriodSchemes/PeriodSchemeRepositoryTests.cs
+++ test/EasyAbp.BookingService.EntityFrameworkCore.Tests/EntityFrameworkCore/PeriodSchemes/PeriodSchemeRepositoryTests.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using EasyAbp.BookingService.PeriodSchemes;
+using Shouldly;
 using Volo.Abp.Domain.Repositories;
 using Xunit;
 
@@ -9,25 +11,50 @@ namespace EasyAbp.BookingService.EntityFrameworkCore.PeriodSchemes
     public class PeriodSchemeRepositoryTests : BookingServiceEntityFrameworkCoreTestBase
     {
         private readonly IPeriodSchemeRepository _periodSchemeRepository;
+        private readonly PeriodSchemeManager _periodSchemeManager;
 
         public PeriodSchemeRepositoryTests()
         {
             _periodSchemeRepository = GetRequiredService<IPeriodSchemeRepository>();
+            _periodSchemeManager = GetRequiredService<PeriodSchemeManager>();
         }
 
-        /*
-        [Fact]
-        public async Task Test1()
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task FindDefaultScheme_Baseline_Test(bool isFound)
         {
+            var entity = await _periodSchemeManager.CreateAsync(nameof(PeriodScheme) + "1", new List<Period>());
+
             await WithUnitOfWorkAsync(async () =>
             {
                 // Arrange
+                var anotherEntity =
+                    await _periodSchemeManager.CreateAsync(nameof(PeriodScheme) + "2", new List<Period>());
+                if (isFound)
+                {
+                    await _periodSchemeManager.SetAsDefaultAsync(entity);
+                }
 
-                // Act
-
-                //Assert
+                await _periodSchemeRepository.InsertManyAsync(new[] { entity, anotherEntity });
             });
+
+
+            // Act
+            var result = await WithUnitOfWorkAsync(() => _periodSchemeRepository.FindDefaultSchemeAsync());
+
+            // Assert
+            if (isFound)
+            {
+                result.ShouldNotBeNull();
+                result.IsDefault.ShouldBeTrue();
+                result.Name.ShouldBe(entity.Name);
+            }
+            else
+            {
+                result.ShouldBeNull();
+            }
         }
-        */
     }
 }
