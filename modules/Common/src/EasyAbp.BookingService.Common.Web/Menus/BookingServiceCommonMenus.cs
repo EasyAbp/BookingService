fe700a46c47a8fce78caab51f0f73ed606b6ecb2
RayMMond <uniquesyp@126.com>
2022-10-07 15:53:01 +0800

    Add menus, add translations

---
+++ modules/Common/src/EasyAbp.BookingService.Common.Web/Menus/BookingServiceCommonMenus.cs
@@ -0,0 +1,21 @@
+﻿namespace EasyAbp.BookingService.Web.Menus;
+
+public static class BookingServiceCommonMenus
+{
+    public const string Prefix = "EasyAbp.BookingService.Common";
+
+    //Add your menu items here...
+    //public const string Home = Prefix + ".MyNewMenuItem";
+
+    public const string AssetCategory = Prefix + ".AssetCategory";
+
+    public const string AssetOccupancy = Prefix + ".AssetOccupancy";
+
+    public const string AssetPeriodScheme = Prefix + ".AssetPeriodScheme";
+
+    public const string Asset = Prefix + ".Asset";
+
+    public const string AssetSchedule = Prefix + ".AssetSchedule";
+
+    public const string PeriodScheme = Prefix + ".PeriodScheme";
+}
